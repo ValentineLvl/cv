a3d477701dd4d293779b4b28f0ef1abece7c6173
Valentine Lavaly <valentinelavaly@MacBook-Air-de-Valentine.local>
2022-01-18 12:13:00 +0100

    changement profession

--- src/components/User.js
+++ src/components/User.js
@@ -12,7 +12,7 @@ function User() {
             <a className='pdf' href='../files/CV-Valentine-LAVALY.pdf' target='blank'><FontAwesomeIcon icon={faFilePdf} size='lg' /></a>
             <img className='avatar' src='../images/photo-cv.png' alt='Avatar' />
             <h1>Valentine Lavaly</h1>    
-            <h2 className='profession'>Développeuse Front-End React</h2>
+            <h2 className='profession'>Développeuse React / Node Junior</h2>
                 <p className='desc'>Créative et organisée : deux qualités qui prouvent que je suis celle qu'il vous faut ! Recherche CDD ou CDI en tant que Développeuse Front-End.</p>
             <h3 className='titre'>Coordonnées</h3>
                 <p className='info'><FontAwesomeIcon className='icon' icon={faPhone} /><a href='tel:+3364837625'>06 48 37 62 05</a></p>
